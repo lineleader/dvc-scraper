a5c1941911056e36c7ffacfab9cb47d38811fdc3
Chris Marshall <chris@codegoalie.com>
2021-01-25 10:57:29 -0500

    Compare scraped prices to current prices

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,27 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/gobuffalo/envy"
 	dvcscraper "github.com/lineleader/dvc-scraper"
 )
+
+var currentPrices = map[string]float64{
+	"Aulani, Disney Vacation Club Villas,\nKo Olina, Hawai‘i":   195,
+	"Disney's Riviera Resort":                                   195,
+	"Copper Creek Villas & Cabins at Disney's Wilderness Lodge": 220,
+	"Bay Lake Tower at Disney's Contemporary Resort":            245,
+	"Boulder Ridge Villas at Disney's Wilderness Lodge":         186,
+	"Disney's Animal Kingdom Villas – Jambo House":              186,
+	"Disney's Animal Kingdom Villas – Kidani Village":           186,
+	"Disney's Beach Club Villas":                                245,
+	"Disney's BoardWalk Villas":                                 210,
+	"Disney's Hilton Head Island Resort":                        135,
+	"Disney's Old Key West Resort":                              165,
+	"Disney's Polynesian Villas & Bungalows":                    250,
+	"Disney's Saratoga Springs Resort":                          165,
+	"Disney's Vero Beach Resort":                                120,
+	"The Villas at Disney's Grand Floridian Resort":             255,
+}
 
 func main() {
 	email := envy.Get("EMAIL", "")
@@ -34,5 +51,17 @@ func main() {
 		log.Fatal(err)
 	}
 
-	spew.Dump(prices)
+	for _, price := range prices {
+		currentPrice, ok := currentPrices[price.Name]
+		if !ok {
+			fmt.Println("\nCurrent price not found!!", price.Name, price.PricePerPoint)
+			continue
+		}
+
+		if currentPrice != price.PricePerPoint {
+			fmt.Printf("Price difference found!!\n%s\n$%.2f => $%.2f\n\n", price.Name, currentPrice, price.PricePerPoint)
+		}
+	}
+
+	fmt.Println("Done.")
 }
