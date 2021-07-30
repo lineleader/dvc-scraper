44446298a67ffdee74523e90e004c7e358272d03
Chris Marshall <chris@codegoalie.com>
2021-07-30 10:38:43 -0400

    Exract sign in to separate file

--- dvcscraper.go
+++ dvcscraper.go
@@ -19,15 +19,6 @@ import (
 )
 
 const (
-	signinURL = "https://disneyvacationclub.disney.go.com/sign-in/"
-
-	dashboardCheckSelector = ".memberNewsAlert"
-	signInBodySelector     = "body#registration_sign_in"
-	signInEmailSelector    = ".field-username-email input"
-	signInPasswordSelector = ".field-password input"
-	signInSubmitSelector   = ".workflow-login .btn-submit"
-	signInErrorSelector    = ".banner.login.message-error.message.state-active"
-
 	cooieSessionFile = ".dvcscraper-session.json"
 )
 
@@ -214,67 +205,6 @@ func (s *Scraper) Screenshot(filename string) error {
 
 	page.MustScreenshotFullPage(filename)
 	return nil
-}
-
-// Login authenticates to gain access to protected parts of the DVC site
-func (s *Scraper) Login() error {
-	page, err := s.getPage()
-	if err != nil {
-		err = fmt.Errorf("failed to get bypass page: %w", err)
-		return err
-	}
-
-	err = page.Navigate(signinURL)
-	if err != nil {
-		err = fmt.Errorf("failed to visit sign in page: %w", err)
-		return err
-	}
-
-	err = page.SetViewport(&proto.EmulationSetDeviceMetricsOverride{
-		Width:  2560,
-		Height: 1400,
-	})
-	if err != nil {
-		err = fmt.Errorf("failed to set viewport: %w", err)
-		return err
-	}
-
-	frame, err := getIFrame(page, `iframe[id="disneyid-iframe"]`)
-	if err != nil {
-		err = fmt.Errorf("failed to get iframe page: %w", err)
-		return err
-	}
-
-	err = typeInput(frame, signInEmailSelector, s.email)
-	if err != nil {
-		err = fmt.Errorf("failed to input email address: %w", err)
-		return err
-	}
-
-	err = typeInput(frame, signInPasswordSelector, s.password)
-	if err != nil {
-		err = fmt.Errorf("failed to input password: %w", err)
-		return err
-	}
-
-	err = click(frame, signInSubmitSelector)
-	if err != nil {
-		err = fmt.Errorf("failed to click to sign in: %w", err)
-		return err
-	}
-
-	err = rod.Try(func() {
-		page.Timeout(10 * time.Second).Race().Element(dashboardCheckSelector)
-	})
-	if errors.Is(err, context.DeadlineExceeded) {
-		signInMsg, err := frame.Element(signInErrorSelector)
-		page.MustScreenshotFullPage("login-error.png")
-		text, _ := signInMsg.Text()
-		err = fmt.Errorf("failed to login: '%s'. See login-error.png for more details.", text)
-		return err
-	}
-
-	return err
 }
 
 func (s *Scraper) AuthenticatedNavigate(url string) error {
